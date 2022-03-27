45ba336acca607ce1ebd320388c9c3ed0a14b9f1
MyktybekOmur <mykty.omurbekov@gmail.com>
2022-03-27 22:47:28 +0300

    home page (added)

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,10 +6,79 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  chartotions: any;
   constructor() { }
 
   ngOnInit(): void {
+    this.chartLine();
   }
+  chartLine(){
+    this.chartotions = {
+      series: [
+        {
+          name: 'Satış',
+          data: [10, 15, 20, 15, 30, 20, 10, 20, 25],
+          color: '#FEB019',
+         
+        },
+      ],
+      stroke: {
+        curve: 'smooth',
+      },
+      markers: {
+        size: 2,
+        colors: undefined,
+        strokeColors: '#fff',
+        strokeWidth: 2,
+        strokeOpacity: 0.9,
+        strokeDashArray: 0,
+        fillOpacity: 1,
+        discrete: [],
+        shape: "circle",
+        radius: 2,
+        offsetX: 0,
+        offsetY: 0,
+        onClick: undefined,
+        onDblClick: undefined,
+        showNullDataPoints: true,
+        hover: {
+          size: undefined,
+          sizeOffset: 3
+        }
+    },
+      chart: {
+        height: 300,
+        width: '100%',
+        type: 'line',
+        toolbar: {
+          show: false,
+          offsetX: 0,
+          offsetY: 0,
+          autoSelected: 'zoom',
+        },
+      },
 
+      dataLabels: {
+        enabled: false,
+      },
+      yaxis: {
+        min: 5,
+        max: 30,
+        tickAmount: 6,
+      },
+      xaxis: {
+        categories: [
+          'Jan',
+          'Feb',
+          'Mar',
+          'Apr',
+          'May',
+          'Jun',
+          'Jul',
+          'Aug',
+          'Sep',
+        ],
+      },
+    };
+  }
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,6 +5,8 @@ import { CommonModule } from '@angular/common';
 import { PagesRoutingModule, routingPageComponents } from './pages-routing.module';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 
 
@@ -17,7 +19,23 @@ import { FooterComponent } from './components/footer/footer.component';
   imports: [
     CommonModule,
     PagesRoutingModule,
-    FormsModule
+    FormsModule,
+    NgApexchartsModule,
+    NgCircleProgressModule.forRoot({
+      backgroundStrokeWidth: 20,
+      backgroundPadding: 40,
+      radius: 36,
+      space: -5,
+      maxPercent: 100,
+      outerStrokeWidth: 8,
+      outerStrokeColor: '#d22828',
+      innerStrokeWidth: 1,
+      titleFontSize: '13',
+      showSubtitle: false,
+      showUnits: false,
+      showBackground: false,
+      lazy: false,
+    }),
   ]
 })
 export class PagesModule { }
