61c467ca42f4aca0db984632231e5ec61b9674c0
MyktybekOmur <mykty.omurbekov@gmail.com>
2022-04-15 22:38:03 +0300

    product page (added)

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -12,4 +12,27 @@ export class ProductsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  deleteUser(id: string){
+
+  }
+  //alertConfirm
+  alertConfirmation(id: string) {
+    Swal.fire({
+      title: 'Silmek ister misiniz?',
+      text: 'Seçmiş olduğunuz veri silinecektir.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Evet',
+      cancelButtonText: 'İptal',
+    }).then((result) => {
+      if (result.value) {
+        this.deleteUser(id); //Swal.fire('Başarılı silindi!', 'success');
+
+      
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        //Swal.fire('Cancelled', 'Product still in our database.)', 'error');
+      }
+    });
+  }
 }
